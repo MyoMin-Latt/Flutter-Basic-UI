1a0cde9711bdaf6c76bed38314cef6707c41ab8d
MyoMinLatt <myominlatt330@gmail.com>
2022-01-26 14:42:45 +0630

    @12.4 Flutter Basic UI (AppBar , Icon, Floating action button)

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 // @12.1 Flutter Set up in Android studio
 // @12.2 Setting up in VS code
 // @12.3 Flutter Basic UI (First App and Widget) => Widget , Material App Widget
+// @12.4 Flutter Basic UI (AppBar , Icon, Floating action button)
 
 import 'package:flutter/material.dart';
 
 //Google => Material 
 //Iphone => Cupertino
-//Widget => less ,ful
+//Widget => stateless, stateful
 //debug //release // hybrid
 void main(){
   runApp(
@@ -22,9 +23,21 @@ class FirstApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+            print('Click');
+        },
+        child: Icon(Icons.access_alarm),
+      ),
+      body: const Center(child: Text('Hello')),
       appBar:  AppBar(
-        
+        centerTitle: true,
+        actions: [
+          Icon(Icons.settings,size: 30,color: Colors.red, ),
+          Center(child: Text('Setting'))
+        ],
+        leading: FlutterLogo(),
+        title: Text('First App'),
       )
     );
   }
