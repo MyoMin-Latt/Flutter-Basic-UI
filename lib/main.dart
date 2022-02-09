d3d53963caf481e041a4ea06d026d4838336fed1
MyoMinLatt <myominlatt330@gmail.com>
2022-02-09 12:17:27 +0630

    @12.10 Flutter Basic UI (Wrap Widget)

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@
 // @12.8 Flutter Basic UI (Rich Text Widget)
 // @12.9 Flutter Basic UI (Row and Column), Expanded, Flexible
 //        Expanded => take all space, Flexible => take needed space
+// @12.10 Flutter Basic UI (Wrap Widget), SingleChildScrollView, For Loop
 
 
 import 'package:flutter/material.dart';
@@ -18,7 +19,7 @@ import 'package:flutter/material.dart';
 //debug //release // hybrid
 //Container
 //project file network memory
-//Row Column Stack ListVeiw GridView
+//Row Column Wrap Stack  ListVeiw GridView
 //variable List Map Set
 // 6 3 3
 void main() {
@@ -30,39 +31,29 @@ void main() {
 
 class FirstApp extends StatelessWidget {
   const FirstApp({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: AppBar(),
-       body: Row(
-         children: [
-           Container(
-             width: 50,
-             height: 200,
-             color: Colors.orange,
-             child: Text('Hello'),
-           ),
-           Expanded(
-             child: Container(
-               width: 50,
-               height: 200,
-               color: Colors.green,
-               child: Text('Hello' *100),
-             ),
-           ),
-           Flexible(
-             child: Container(  
-               color: Colors.red,
-               child: Text('Hello'),
-               
-             ),
-           ),
-         ],
-       )
-
-      
-       );
+        appBar: AppBar(),
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.black,
+          child: SingleChildScrollView(
+            child: Wrap(
+              textDirection: TextDirection.ltr,
+              direction: Axis.horizontal,
+              verticalDirection: VerticalDirection.down,
+              spacing: 8.0,
+              runSpacing: 38.0,
+              alignment: WrapAlignment.center,
+              children: [
+                for (int i = 0; i < 100; i++)
+                  ElevatedButton(onPressed: () {}, child: Text('Button $i'))
+              ],
+            ),
+          ),
+        ));
   }
 }
-
